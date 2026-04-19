d1d04faeeb5282f6e5dfc6d128f1d8b9b26a2ec3
Marlies Dens <marlies.dens@pxl.be>
2026-04-19 09:59:27 +0200

    1.2 tem 1.8

---
+++ LaboPokémon/Pokemon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboPokémon
+{
+    internal class Pokemon
+    {
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		private int _level;
+
+		public int Level
+		{
+			get { return _level; }
+			set { _level = value; }
+		}
+
+		private string _type;
+
+		public string Type
+		{
+			get { return _type; }
+			set { _type = value; }
+		}
+
+        public Pokemon()
+        {
+            
+        }
+
+        public Pokemon(string name, string type)
+        {
+			_name = name;
+			_type = type;
+        }
+
+
+    }
+}
--- LaboPokémon/Program.cs
+++ LaboPokémon/Program.cs
@@ -4,7 +4,54 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] pokémon = new string[4];
+            pokémon[0] = "Pikachu";
+            pokémon[1] = "Bulbasaur";
+            pokémon[2] = "Charmander";
+            pokémon[3] = "Squirtle";
+
+            Console.WriteLine(pokémon[0]);
+
+            Array.Clear(pokémon, 1, 1);
+
+            PrintPokemonRoster(pokémon);
+
+            Console.WriteLine();
+
+            bool isValid = IsValidPokemonRosterSize(pokémon);
+            Console.WriteLine(isValid);
+
+            Pokemon charmeleon = new Pokemon("Charmeleon", "Fire");
+            Pokemon raichu = new Pokemon();
+            raichu.Name = "Raichu";
+            raichu.Type = "Electric";
+            Pokemon golem = new Pokemon();
+            golem.Name = "Golem";
+            golem.Type = "Rock";
+            Pokemon eevee = new Pokemon();
+            eevee.Name = "Eevee";
+            eevee.Type = "Normal";        
+            Pokemon bonsly = new Pokemon("Bonsly", "Rock");            
+            Pokemon treecko = new Pokemon("Treecko", "Grass");
+
+            Pokemon[] pokémon2 = new Pokemon[] {charmeleon, raichu, golem, eevee, bonsly, treecko};
+            
+        }
+
+        static void PrintPokemonRoster(string[] names)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+
+        static bool IsValidPokemonRosterSize(string[] names)
+        {
+            if (names.Length == 6)
+                return true;
+            else
+                return false;
         }
     }
 }

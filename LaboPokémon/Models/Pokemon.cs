5d602982af4d142f7885e82729df104033ea4971
Marlies Dens <marlies.dens@pxl.be>
2026-04-21 09:24:14 +0200

    klasse pokemon verbeterd

--- LaboPokémon/Models/Pokemon.cs
+++ LaboPokémon/Models/Pokemon.cs
@@ -8,40 +8,44 @@ namespace LaboPokémon.Models
 {
     public class Pokemon
     {
-		private string _name;
-
-		public string Name
-		{
-			get { return _name; }
-			set { _name = value; }
-		}
-
-		private int _level;
-
-		public int Level
-		{
-			get { return _level; }
-			set { _level = value; }
-		}
-
-		private string _type;
-
-		public string Type
-		{
-			get { return _type; }
-			set { _type = value; }
-		}
-
-        public Pokemon()
+        public Pokemon(string name, string type, int level)
         {
-            
+            Name = name;
+            Type = type;
+            Level = level;
         }
 
-        public Pokemon(string name, string type)
-        {
-			_name = name;
-			_type = type;
-        }
+        
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Level { get; set; }
+
+        //private string _name;
+
+        //public string Name
+        //{
+        //	get { return _name; }
+        //	set { _name = value; }
+        //}
+
+        //private int _level;
+
+        //public int Level
+        //{
+        //	get { return _level; }
+        //	set { _level = value; }
+        //}
+
+        //private string _type;
+
+        //public string Type
+        //{
+        //	get { return _type; }
+        //	set { _type = value; }
+        //}
+
+
 
 
     }
